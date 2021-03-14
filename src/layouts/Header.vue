a24bbeb58d5c1ad7a9b1b6b8b6d7fdf71d4e5a2d
유성실 <yooo1201@naver.com>
2021-03-14 22:18:08 +0900

    💄 헤더, css

---
+++ src/layouts/Header.vue
@@ -0,0 +1,49 @@
+<template>
+	<v-app-bar app color="white" flat>
+		<v-container class="py-0 fill-height">
+			<v-avatar class="mr-10" color="teal" size="32">
+				<span class="white--text">데체</span>
+			</v-avatar>
+
+			<v-btn v-for="link in links" :key="link.name" text>
+				<router-link :to="link.to">{{ link.name }}</router-link>
+			</v-btn>
+
+			<v-spacer />
+
+			<v-responsive max-width="260">
+				<v-text-field
+					prepend-inner-icon="mdi-magnify"
+					filled
+					rounded
+					dense
+					hide-details
+					color="grey"
+				/>
+			</v-responsive>
+		</v-container>
+	</v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	data() {
+		return {
+			links: [
+				{
+					name: 'Home',
+					to: '/'
+				},
+				{
+					name: 'About',
+					to: '/about'
+				}
+			]
+		};
+	}
+});
+</script>
+
+<style></style>
