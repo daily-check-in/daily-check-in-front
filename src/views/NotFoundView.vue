40752f2b71e9b0af4b9864ae3c0115f006e749bc
유성실 <yooo1201@naver.com>
2021-04-14 02:10:07 +0900

    💄 404 페이지

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,13 +1,15 @@
 <template>
-	<v-row class="d-flex align-center">
-		<v-col sm="6" offset-sm="3">
-			<div class="d-flex flex-column pa-4">
-				<img src="../assets/images/404.svg" alt class="mb-10" />
+	<v-container>
+		<v-row class="d-flex align-center">
+			<v-col sm="6" offset-sm="3">
+				<div class="d-flex flex-column pa-4">
+					<img src="../assets/images/404.svg" alt class="mb-10" />
 
-				<v-btn to="/" outlined color="success">메인으로 돌아가기</v-btn>
-			</div>
-		</v-col>
-	</v-row>
+					<v-btn to="/" outlined color="success">메인으로 돌아가기</v-btn>
+				</div>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script lang="ts">
