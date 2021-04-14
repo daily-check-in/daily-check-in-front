52fb03eb93c4e01ee30a2edf4ba2efc334600e1a
유성실 <yooo1201@naver.com>
2021-04-15 00:55:48 +0900

    🔧 head title 변경

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	devServer: {
 		overlay: false
+	},
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].title = '데일리 체크인';
+			return args;
+		});
 	}
 };
