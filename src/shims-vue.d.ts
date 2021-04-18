7df6cc629c780e1623f23b382341f1cb15f7c23e
유성실 <yooo1201@naver.com>
2021-04-18 15:39:59 +0900

    🔨 declaration file (vue-quill-editor)

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,7 @@ declare module '*.vue' {
 	import Vue from 'vue';
 	export default Vue;
 }
+declare module 'vue-quill-editor' {
+	import { quillEditor } from 'vue-quill-editor';
+	export { quillEditor };
+}
